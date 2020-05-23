dc9780a69729545591d6d1531076e7fe29efc25e
Dinamokid dinamokidov <dinamokid@yandex.ru>
2020-05-23 09:57:30 +0700

    set cdn to npm package

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 import App from './App.vue'
 
 Vue.use(VueRouter)
